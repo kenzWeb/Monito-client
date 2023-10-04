9b9a73d52cc5a3eb4958780169ea232ccbabbffa
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2023-10-04 23:34:29 +0300

    Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,21 +7,40 @@ const Main = () => {
 				<div className='main__header'>
 					<div className='main__header-left'>
 						<h2 className='main__header-title'>One more friend</h2>
-						<h3 className='main__header-subtitle'>Thousands more fun!</h3>
-						<p className='main__header-description'>
+						<h3 className='main__header-subtitle Bold46'>Thousands more fun!</h3>
+						<p className='main__header-description Med16'>
 							Having a pet means you have more joy, a new friend, a happy person
 							who will always be with you to have fun. We have 200+ different
 							pets that can meet your needs!
 						</p>
 						<div className='main__buttons'>
 							<button className='main__button-view-intro'>
-								<h2>View Intro</h2>
-								<img src='img/images/main/maindog.svg' alt='' />
+								<h2 className='mr-[8px]'>View Intro</h2>
+								<svg
+									xmlns='http://www.w3.org/2000/svg'
+									fill='none'
+								>
+									<path
+										d='M10 9V15L15 12L10 9Z'
+										fill='#003459'
+										stroke='#003459'
+										stroke-width='3'
+										stroke-linecap='round'
+										stroke-linejoin='round'
+									/>
+								</svg>
 							</button>
 							<button className='main__button-explore-now'>
 								<h2>Explore Now</h2>
 							</button>
 						</div>
+					</div>
+					<div className='main__header-right'>
+						<img
+							className='main__header-img'
+							src='img/images/main/maindog.png'
+							alt=''
+						/>
 					</div>
 				</div>
 			</div>
--- src/pages/HomePages.jsx
+++ src/pages/HomePages.jsx
@@ -4,7 +4,7 @@ import Main from '../components/Main/Main'
 
 const HomePages = () => {
 	return (
-		<div className='home_page'>
+		<div className='home_page z-10'>
 			<Header />
 			<Main />
 		</div>
